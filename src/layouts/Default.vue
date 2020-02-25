dcac0f469d8e7bb0996830579e6a554dad7f4ce4
Marcel Behrmann <marcel.behrmann@real-digital.de>
2020-02-25 14:06:27 +0100

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-content>
+            Home
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-content>
+            About
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ $static.metadata.siteName }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <slot/>
+    </v-content>
+
+    <v-footer app>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+  })
+}
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <g-image alt="gridsome logo" src="~/favicon.png" width="135" />
+            <g-image alt="vuetify logo" src="~/vuetify-logo-dark.webp" width="135" />
+          </v-col>
+        </v-row>
+      </v-container>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
